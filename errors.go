740573d77a52b101051a6e1efa8f1bd06cb909f9
henderjon <list@box370.net>
2019-04-10 08:29:40 -0500

    marshal is now serialize

--- errors.go
+++ errors.go
@@ -1,22 +1,24 @@
 package errors
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 )
 
-const delim = "\n"
-const unitsep = ";\037" // []byte{31} is the ascii Unit Separator (US) character
+// Delim and UnitSep are used to separate/delimit fields
+const (
+	Delim   = "\n"
+	UnitSep = ";\037" // []byte{31} is the ascii Unit Separator (US) character
+)
 
 // Error is an error with an embedded "previous" error
 type Error struct {
 	err  string
-	prev *Error
+	prev error
 }
 
 // New creates a new Error of our own liking
-func New(args ...interface{}) *Error {
+func New(args ...interface{}) error {
 	if len(args) == 0 {
 		log.Fatal("call to errors.New with no arguments")
 	}
@@ -35,33 +37,64 @@ func New(args ...interface{}) *Error {
 }
 
 // Prev allows access to an Error's prev
-func (e *Error) Prev() *Error {
+func (e *Error) Prev() error {
 	return e.prev
 }
 
 // Error fulfills the error interface
 func (e *Error) Error() string {
-	return string(e.Marshal(delim))
+	return string(Serialize(e, nil, Delim))
 }
 
 // Error fulfills the stringer interface
 func (e *Error) String() string {
-	return string(e.Marshal("; "))
+	return string(Serialize(e, nil, "; "))
 }
 
-// Marshal writes the entire stack using sep as a delimeter
-func (e *Error) Marshal(sep string) []byte {
-	var b bytes.Buffer
-	b.WriteString(e.err)
+// Serialize writes the entire stack using sep as a delimeter
+func (e *Error) Serialize(b []byte, sep string) []byte {
+	b = append(b, e.err...)
 	if e.prev != nil {
-		b.WriteString(sep)
-		b.Write(e.prev.Marshal(sep))
+		b = append(b, sep...)
+		b = Serialize(e.prev, b, sep)
 	}
-	return b.Bytes()
+	return b
+}
+
+// Serialize writes the entire stack using sep as a delimeter
+func Serialize(err error, b []byte, sep string) []byte {
+	if err == nil {
+		return nil
+	}
+	if e, ok := err.(*Error); ok {
+		return e.Serialize(b, sep)
+	}
+	b = append(b, err.Error()...)
+	return b
+}
+
+// Pulled from https://godoc.org/upspin.io/errors
+
+// Recreate the errors.New functionality of the standard Go errors package
+// so we can create simple text errors when needed.
+
+// Str returns an error that formats as the given text. It is intended to
+// be used as the error-typed argument to the E function.
+func Str(text string) error {
+	return &errorString{text}
+}
+
+// errorString is a trivial implementation of error.
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
 }
 
 // Errorf is equivalent to fmt.Errorf, but allows clients to import only this
-// package for all error handling. Pulled from https://godoc.org/upspin.io/errors
+// package for all error handling.
 func Errorf(format string, args ...interface{}) error {
-	return New(fmt.Sprintf(format, args...))
+	return &errorString{fmt.Sprintf(format, args...)}
 }
--- errors_test.go
+++ errors_test.go
@@ -12,13 +12,15 @@ func TestErrors(t *testing.T) {
 		"fourth",
 	}
 
-	tmpE := New("")
+	tmpE := New("OH NOES!")
 	for _, e := range errs {
 		tmpE = New(e, tmpE)
 	}
 
-	if len(tmpE.Error()) != len(tmpE.Marshal("\t")) {
+	if len(tmpE.Error()) != len(Serialize(tmpE, nil, "-")) {
 		t.Fatal("Error and Marshal should be of same len()")
 	}
+
+	// fmt.Println(tmpE)
 
 }
